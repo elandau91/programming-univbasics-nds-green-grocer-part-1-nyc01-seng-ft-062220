6c085c5da165001fb347f7265dd7a5be91cf67f4
Eli Landau <eli.b.landau@gmail.com>
2020-06-19 16:05:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,6 +19,7 @@ def consolidate_cart(cart)
   
   cart.each do |grocery_item|
     current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    binding.pry
     if current_item
       new_cart_index = 0
       new_cart.each do |new_cart_item|
